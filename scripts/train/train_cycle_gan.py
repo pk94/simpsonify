26823edb250ee05c57dd0873c3feb5340c4369bc
pk94 <kowaleczko.p@gmail.com>
2020-03-31 21:15:43 +0200

    Changes

--- scripts/train/train_cycle_gan.py
+++ scripts/train/train_cycle_gan.py
@@ -109,9 +109,9 @@ def train_loop(metafile_path, checkpoint_path, num_epochs=50):
                        generator_g_optimizer, generator_f_optimizer, discriminator_x_optimizer,
                        discriminator_y_optimizer)
             clear_output(wait=True)
-            if n % 100 == 0:
+            if n % 50 == 0:
                 generate_images(generator_g, 'zdjecie.jpg')
-            if n % 500 == 0:
+            if n % 1000 == 0:
                 print(f'Epoch: {epoch}, step: {n}')
                 ckpt_save_path = ckpt_manager.save()
                 print('Saving checkpoint for epoch {} at {}'.format(epoch, ckpt_save_path))
