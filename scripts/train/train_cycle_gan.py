f087b6ed786db5ad9b1ee47c7e63a4d675c9ac90
pk94 <kowaleczko.p@gmail.com>
2020-03-31 20:41:30 +0200

    Changes

--- scripts/train/train_cycle_gan.py
+++ scripts/train/train_cycle_gan.py
@@ -97,10 +97,11 @@ def train_loop(metafile_path, checkpoint_path, num_epochs=50):
                                discriminator_x_optimizer=discriminator_x_optimizer,
                                discriminator_y_optimizer=discriminator_y_optimizer)
 
-    ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=1)
-    if ckpt_manager.latest_checkpoint:
-        ckpt.restore(ckpt_manager.latest_checkpoint)
-        print('Latest checkpoint restored!!')
+    ckpt.restore(checkpoint_path)
+    # ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=1)
+    # if ckpt_manager.latest_checkpoint:
+    #     ckpt.restore(ckpt_manager.latest_checkpoint)
+    #     print('Latest checkpoint restored!!')
 
     for epoch in range(num_epochs):
         n = 0
@@ -113,8 +114,8 @@ def train_loop(metafile_path, checkpoint_path, num_epochs=50):
                 generate_images(generator_g, 'zdjecie.jpg')
             if n % 500 == 0:
                 print(f'Epoch: {epoch}, step: {n}')
-                ckpt_save_path = ckpt_manager.save()
-                print('Saving checkpoint for epoch {} at {}'.format(epoch, ckpt_save_path))
+                ckpt.write(checkpoint_path + 'ckpt')
+                print('Saving checkpoint for epoch {}'.format(epoch))
             n += 1
 
 
